2344785adec4b91a9ae903710631f5abe4608642
Man Hong Lee <manhong323@gmail.com>
2017-10-18 19:19:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var katzDeli = [];
-
+var indexNum = 0;
 function takeANumber(katzDeliLine, name){
-
+  indexNum++;
+  katzDeliLine.push(`Welcome, ${name}. You are number ${indexNum}`);
+  return katzDeliLine;
 }
